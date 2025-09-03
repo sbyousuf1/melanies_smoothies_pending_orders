4032241e47c3c58e5bdd764c49256a930ea4997c
sbyousuf1 <yousuf.bilal@gmail.com>
2025-09-03 14:49:43 +1000

    Update streamlit_app.py
    updated connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+cnx = st.connection("snowflake")
 from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
@@ -10,7 +10,7 @@ st.write(
   """Orders that need to be filled."""
 )
 
-session = get_active_session()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 
 if my_dataframe:
